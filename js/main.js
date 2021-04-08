a15e9e5ec651d9a12c87d705c5ce9469cfdebb99
Prathamkrishna <pratham.krishna.06@gmail.com>
2021-04-08 16:45:41 +0530

    Added files

---
+++ js/main.js
@@ -0,0 +1,41 @@
+function taketodocs() {
+    window.open('https://docs.google.com/document/d/1FXJx9CgTUu2VYwuBFwcVD9CRxaa-8fqaGSiGS72anxA/edit?usp=sharing', '_blank');
+}
+
+function lightmodeconv() {
+    document.getElementById("wholesection").style.cssText = 'background-color:aqua;  color:black';
+    document.getElementById("bodycolor").style.cssText = 'background-color: aqua';
+    document.getElementById("wholebottom").style.cssText = 'background-color:white; color:black';
+    document.getElementById("linkcolor").style.cssText = 'color: black; text-decoration: none';
+    document.getElementById("rightmiddletext").style.cssText = 'font-weight: 700';
+    document.getElementById("sendemail").style.cssText = 'color:black; border-color:black';
+}
+
+function darkmodeconv() {
+    document.getElementById("wholesection").style.cssText = 'background-color:black; color:white';
+    document.getElementById("bodycolor").style.cssText = 'background-color:black';
+    document.getElementById("wholebottom").style.cssText = 'background-color: #282828; color:white';
+    document.getElementById("linkcolor").style.cssText = 'color:white; text-decoration: none';
+    document.getElementById("rightmiddletext").style.cssText = 'font-weight: 300';
+    document.getElementById("sendemail").style.cssText = 'color:white; border-color:white; border-width:1px';
+}
+
+function taketolinkedin() {
+    window.open('https://www.linkedin.com/in/pratham-krishna-2a4993145/', '_blank');
+}
+
+function taketogithub() {
+    window.open('https://github.com/Prathamkrishna', '_blank');
+}
+
+function taketoinstagram() {
+    window.open('https://instagram.com/prathamkrishna', '_blank');
+}
+
+function taketoyoutube() {
+    window.open('https://www.youtube.com/channel/UCfkny3ccbpadisVQ0-k7YVQ', '_blank')
+}
+
+function sendanemail() {
+    window.open('https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSKjDPrvnnHjFTHrrRcfgTMDDwTgKDgXhsQMrctxmqkspSkMRxKKzgdhNcrfbVZcxZDfZLKq', '_blank');
+}
